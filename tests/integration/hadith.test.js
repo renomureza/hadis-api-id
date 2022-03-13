929003395cf52e6ea59c7c69dc1ab5633341fa80
renomureza <renomureza@gmail.com>
2022-03-13 17:45:17 +0700

    fix test

--- tests/integration/hadith.test.js
+++ tests/integration/hadith.test.js
@@ -2,16 +2,16 @@ const request = require('supertest');
 const app = require('../../src/app');
 const httpStatus = require('../../src/utils/httpStatus');
 
-describe('GET /v1/hadith', () => {
+describe('GET /hadith', () => {
   describe('when correct path', () => {
     test('should return http status 200', async () => {
-      const res = await request(app).get('/v1/hadith');
+      const res = await request(app).get('/hadith');
 
       expect(res.statusCode).toBe(httpStatus.OK);
     });
 
     test('should return correct array of object', async () => {
-      const res = await request(app).get('/v1/hadith');
+      const res = await request(app).get('/hadith');
 
       expect(res.statusCode).toBe(httpStatus.OK);
       res.body.forEach((narrator) => {
@@ -26,13 +26,13 @@ describe('GET /v1/hadith', () => {
 
   describe('when wrong path', () => {
     test('should return http status 404', async () => {
-      const res = await request(app).get('/v1/hadithh');
+      const res = await request(app).get('/hadithh');
 
       expect(res.statusCode).toBe(httpStatus.NOT_FOUND);
     });
 
     test('should return error object', async () => {
-      const res = await request(app).get('/v1/hadithh');
+      const res = await request(app).get('/hadithh');
 
       expect(res.body).toMatchObject({
         message: expect.any(String),
@@ -42,16 +42,16 @@ describe('GET /v1/hadith', () => {
   });
 });
 
-describe('GET /v1/hadith/:narrator', () => {
+describe('GET /hadith/:narrator', () => {
   describe('when correct path', () => {
     test('should return http status 200', async () => {
-      const res = await request(app).get('/v1/hadith/abu-dawud');
+      const res = await request(app).get('/hadith/abu-dawud');
 
       expect(res.statusCode).toBe(httpStatus.OK);
     });
 
     test('should return correct object', async () => {
-      const res = await request(app).get('/v1/hadith/abu-dawud');
+      const res = await request(app).get('/hadith/abu-dawud');
 
       expect(res.body).toMatchObject({
         name: expect.any(String),
@@ -81,14 +81,14 @@ describe('GET /v1/hadith/:narrator', () => {
 
   describe('when limit page size 50', () => {
     test('should return 50 items', async () => {
-      const res = await request(app).get('/v1/hadith/abu-dawud?limit=50');
+      const res = await request(app).get('/hadith/abu-dawud?limit=50');
       expect(res.body.items.length).toBe(50);
     });
   });
 
   describe('when page out of total page', () => {
     test('should return http status 404', async () => {
-      const res = await request(app).get('/v1/hadith/abu-dawud?page=1000');
+      const res = await request(app).get('/hadith/abu-dawud?page=1000');
 
       expect(res.statusCode).toBe(404);
       expect(res.body).toMatchObject({
@@ -99,16 +99,16 @@ describe('GET /v1/hadith/:narrator', () => {
   });
 });
 
-describe('GET /v1/hadith/:narrator/:number', () => {
+describe('GET /hadith/:narrator/:number', () => {
   describe('when number hadits is exists', () => {
     test('should return http status 200', async () => {
-      const res = await request(app).get('/v1/hadith/abu-dawud/1');
+      const res = await request(app).get('/hadith/abu-dawud/1');
 
       expect(res.statusCode).toBe(httpStatus.OK);
     });
 
     test('should return correct object', async () => {
-      const res = await request(app).get('/v1/hadith/abu-dawud/1');
+      const res = await request(app).get('/hadith/abu-dawud/1');
 
       expect(res.body).toMatchObject({
         name: expect.any(String),
@@ -121,13 +121,13 @@ describe('GET /v1/hadith/:narrator/:number', () => {
 
   describe('when number hadits not exists', () => {
     test('should return http status 200', async () => {
-      const res = await request(app).get('/v1/hadith/abu-dawud/0');
+      const res = await request(app).get('/hadith/abu-dawud/0');
 
       expect(res.statusCode).toBe(httpStatus.NOT_FOUND);
     });
 
     test('should return correct object', async () => {
-      const res = await request(app).get('/v1/hadith/abu-dawud/0');
+      const res = await request(app).get('/hadith/abu-dawud/0');
 
       expect(res.body).toMatchObject({
         message: expect.any(String),
